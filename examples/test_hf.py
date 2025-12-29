026b684abe618f6f7fd0c199f41525a60e5ec93f
Heng Ma <maheng1991@hotmail.com>
2025-12-29 14:25:16 -0600

    added hf test

---
+++ examples/test_hf.py
@@ -0,0 +1,33 @@
+from llm_inference.llm_interface import huggingfaceLLM
+import logging
+
+# Configure logger
+logger = logging.getLogger(__name__)
+
+llm = huggingfaceLLM(model="openai/gpt-oss-20b")
+
+prompt = "application of machine learning in cancer diagnosis"
+
+
+schema = {
+            "hypothesis": {
+                "title": "string",
+                "content": "string",
+                "summary": "string",
+                "key_novelty_aspects": ["string"],
+                "testable_predictions": ["string"]
+            },
+            "explanation": "string",
+            "generation_strategy": "string"
+        }
+
+
+responese, _, _ = llm.generate_with_json_output(
+    prompt=prompt,
+    json_schema=schema,
+    max_tokens=5000,
+)
+
+print(responese)
+
+
